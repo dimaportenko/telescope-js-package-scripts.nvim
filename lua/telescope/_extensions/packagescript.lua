7828c56e51f61945a71af148d565ef4c8873aa8e
Neil Messelmani <neil.messelmani@bisimulations.com>
2022-06-03 23:37:42 +0200

    Just work

--- lua/telescope/_extensions/packagescript.lua
+++ lua/telescope/_extensions/packagescript.lua
@@ -3,7 +3,6 @@ local state = require('telescope.actions.state')
 local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
 local sorters = require('telescope.sorters')
-local Terminal = require("toggleterm.terminal").Terminal
 
 return require('telescope').register_extension {
     exports = {
@@ -38,7 +37,7 @@ return require('telescope').register_extension {
                     local execute_script = function()
                         local selection = state.get_selected_entry(prompt_bufnr)
                         actions.close(prompt_bufnr)
-                        local command = 'Terminal:new({ cmd = "' .. scriptsFromJson[selection.value] .. '", hidden = true }):toggle()'
+                        local command = 'require("toggleterm.terminal").Terminal:new({ cmd = "' .. scriptsFromJson[selection.value] .. '", hidden = true }):toggle()'
                         vim.cmd(command)
                     end
 
