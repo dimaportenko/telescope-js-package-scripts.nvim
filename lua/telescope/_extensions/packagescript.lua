6462a2b50834427a0f4ba458ac3bad48e9975711
Dima Portenko <troublesohard@gmail.com>
2022-10-13 22:27:52 +0300

    fix: package.json path for OS X

--- lua/telescope/_extensions/packagescript.lua
+++ lua/telescope/_extensions/packagescript.lua
@@ -6,48 +6,47 @@ local sorters = require('telescope.sorters')
 local Terminal = require("toggleterm.terminal").Terminal
 
 return require('telescope').register_extension {
-    exports = {
-        scripts = function(opts)
-            opts = opts or {}
+  exports = {
+    scripts = function(opts)
+      opts = opts or {}
 
-            local filePath   = vim.fn.getcwd() .. '\\package.json'
+      local filePath = vim.fn.getcwd() .. '/package.json'
 
-            local file       = io.open(filePath, "rb")
-            if file == nil then
-                error("Package.json could not be found")
-            end
-            local jsonString = file:read "*a"
-            file:close()
+      local file = io.open(filePath, "rb")
+      if file == nil then
+        error("Package.json could not be found")
+      end
+      local jsonString = file:read "*a"
+      file:close()
 
-            local scriptsFromJson = vim.fn.json_decode(jsonString)['scripts']
+      local scriptsFromJson = vim.fn.json_decode(jsonString)['scripts']
 
-            local scriptsNames = {}
-            local scripts      = {}
-            for name, code in pairs(scriptsFromJson) do
-                table.insert(scriptsNames , name)
-                table.insert(scripts      , code)
-            end
+      local scriptsNames = {}
+      local scripts      = {}
+      for name, code in pairs(scriptsFromJson) do
+        table.insert(scriptsNames, name)
+        table.insert(scripts, code)
+      end
 
-            pickers.new(opts, {
-                prompt_title = 'Scripts',
-                finder = finders.new_table {
-                    results = scriptsNames
-                },
-                sorter = sorters.get_generic_fuzzy_sorter(),
-                attach_mappings = function(prompt_bufnr, map)
-                    local execute_script = function()
-                        local selection = state.get_selected_entry(prompt_bufnr)
-                        actions.close(prompt_bufnr)
-                        Terminal:new({ cmd = "' .. scriptsFromJson[selection.value] .. '", hidden = true }):toggle()
-                    end
+      pickers.new(opts, {
+        prompt_title = 'Scripts',
+        finder = finders.new_table {
+          results = scriptsNames
+        },
+        sorter = sorters.get_generic_fuzzy_sorter(),
+        attach_mappings = function(prompt_bufnr, map)
+          local execute_script = function()
+            local selection = state.get_selected_entry(prompt_bufnr)
+            actions.close(prompt_bufnr)
+            Terminal:new({ cmd = "' .. scriptsFromJson[selection.value] .. '", hidden = true }):toggle()
+          end
 
-                    map('i', '<CR>', execute_script)
-                    map('n', '<CR>', execute_script)
+          map('i', '<CR>', execute_script)
+          map('n', '<CR>', execute_script)
 
-                    return true
-                end
-            }):find()
+          return true
         end
-    }
+      }):find()
+    end
+  }
 }
-
